3e2d1e5681a54a21c5572e05bf4aefbf85732453
Jens W. Klein <jens@bluedynamics.com>
2010-12-13 15:46:23 +0000

    added some svn:ignores | updated fullcalendar to official 1.4.9 | make div id to bind on more unique and remove superfluos try

--- zettwerk/fullcalendar/browser/javascript/enable.js
+++ zettwerk/fullcalendar/browser/javascript/enable.js
@@ -1,7 +1,3 @@
 $(document).ready(function() {
-    try {
-	$('#calendar').fullCalendar(defaultCalendarOptions);
-    } catch(e) {
-	null; // ignore views, that dont use the calendar
-    }
+    $('#jquery-fullcalendar').fullCalendar(defaultCalendarOptions);
 });
